1002ba7c86b726ba4ab9438623588efdf66521f9
Quarx2k <agent00791@gmail.com>
2011-11-30 06:28:24 +0700

    fix GPU rev2

--- services/surfaceflinger/DisplayHardware/DisplayHardware.cpp
+++ services/surfaceflinger/DisplayHardware/DisplayHardware.cpp
@@ -112,14 +112,31 @@ static status_t selectConfigForPixelFormat(
     EGLConfig* const configs = new EGLConfig[numConfigs];
     eglChooseConfig(dpy, attrs, configs, numConfigs, &n);
     for (int i=0 ; i<n ; i++) {
-/*        EGLint nativeVisualId = 0;
-        eglGetConfigAttrib(dpy, configs[i], EGL_NATIVE_VISUAL_ID, &nativeVisualId);
+
+/*		EGLint nativeVisualId = 0;
+        eglGetConfigAttrib(dpy, configs, EGL_NATIVE_VISUAL_ID, &nativeVisualId);
+		LOGI("selectConfigForPixelFormat %x %x", format, nativeVisualId);		
         if (nativeVisualId>0 && format == nativeVisualId) {
+            *outConfig = configs;
+            delete [] configs;
+            return NO_ERROR;
+        }*/
+
+        EGLint r,g,b,a;
+        EGLConfig curr = configs[i];
+        eglGetConfigAttrib(dpy, curr, EGL_RED_SIZE,   &r);
+        eglGetConfigAttrib(dpy, curr, EGL_GREEN_SIZE, &g);
+        eglGetConfigAttrib(dpy, curr, EGL_BLUE_SIZE,  &b);
+        eglGetConfigAttrib(dpy, curr, EGL_ALPHA_SIZE, &a);
+	LOGI("RGBA %x %x %x %x ", r,g,b,a);	
+	LOGI("KAI2 selectConfigForPixelFormat %x", i);	
+//        if (8 == a && 8 == r && 8 == g && 8  == b) {
+        if (8 == a && 8 == r && 8 == g && 8  == b) {
+	LOGI("KAI selectConfigForPixelFormat %x", i);		
             *outConfig = configs[i];
             delete [] configs;
             return NO_ERROR;
         }
-*/
     }
     delete [] configs;
     return NAME_NOT_FOUND;
