3591a6ebd5ead29493da9b61b402e9ace1b20a00
Tanguy Pruvot <tanguy.pruvot@gmail.com>
2012-03-06 07:42:04 +0100

    MemoryHeapBase: ifdef for gingerbread compatibility
    Allow to "revert" by a board config flag the offset member addon
    see commit f24c4cd0f20 (aosp change id Ie618fb5c0718e)
    
    This change is required to use overlay based proprietary camera libs
    coming from gingerbread and froyo builds.
    
    This change also requires a global cflag named BINDER_COMPAT
    because a lot of modules use the MemoryHeap includes
    
    Add this in your BoardConfig.mk :
    COMMON_GLOBAL_CFLAGS += -DBINDER_COMPAT
    
    Change-Id: Ib0370fbf4c4770f8141607a8fc393639ffbdc8e2

--- libs/binder/MemoryHeapBase.cpp
+++ libs/binder/MemoryHeapBase.cpp
@@ -42,13 +42,19 @@ namespace android {
 
 MemoryHeapBase::MemoryHeapBase()
     : mFD(-1), mSize(0), mBase(MAP_FAILED),
-      mDevice(NULL), mNeedUnmap(false), mOffset(0)
+      mDevice(NULL), mNeedUnmap(false)
+#ifndef BINDER_COMPAT
+    , mOffset(0)
+#endif
 {
 }
 
 MemoryHeapBase::MemoryHeapBase(size_t size, uint32_t flags, char const * name)
     : mFD(-1), mSize(0), mBase(MAP_FAILED), mFlags(flags),
-      mDevice(0), mNeedUnmap(false), mOffset(0)
+      mDevice(0), mNeedUnmap(false)
+#ifndef BINDER_COMPAT
+    , mOffset(0)
+#endif
 {
     //LOGD("%s: size=%d, flags=%x, name=%s", __FUNCTION__, size, flags, name);
     const size_t pagesize = getpagesize();
@@ -66,7 +72,10 @@ MemoryHeapBase::MemoryHeapBase(size_t size, uint32_t flags, char const * name)
 
 MemoryHeapBase::MemoryHeapBase(const char* device, size_t size, uint32_t flags)
     : mFD(-1), mSize(0), mBase(MAP_FAILED), mFlags(flags),
-      mDevice(0), mNeedUnmap(false), mOffset(0)
+      mDevice(0), mNeedUnmap(false)
+#ifndef BINDER_COMPAT
+    , mOffset(0)
+#endif
 {
     //LOGD("%s: device=%s, size=%d, flags=%x", __FUNCTION__, device, size, flags);
     int open_flags = O_RDWR;
@@ -86,7 +95,10 @@ MemoryHeapBase::MemoryHeapBase(const char* device, size_t size, uint32_t flags)
 
 MemoryHeapBase::MemoryHeapBase(int fd, size_t size, uint32_t flags, uint32_t offset)
     : mFD(-1), mSize(0), mBase(MAP_FAILED), mFlags(flags),
-      mDevice(0), mNeedUnmap(false), mOffset(0)
+      mDevice(0), mNeedUnmap(false)
+#ifndef BINDER_COMPAT
+    , mOffset(0)
+#endif
 {
     //LOGD("%s: fd=%d, size=%d, flags=%x, offset=%d", __FUNCTION__, fd, size, flags, offset);
     const size_t pagesize = getpagesize();
@@ -156,7 +168,9 @@ status_t MemoryHeapBase::mapfd(int fd, size_t size, uint32_t offset)
     }
     mFD = fd;
     mSize = size;
+#ifndef BINDER_COMPAT
     mOffset = offset;
+#endif
     return NO_ERROR;
 }
 
@@ -199,9 +213,11 @@ const char* MemoryHeapBase::getDevice() const {
     return mDevice;
 }
 
+#ifndef BINDER_COMPAT
 uint32_t MemoryHeapBase::getOffset() const {
     return mOffset;
 }
+#endif
 
 // ---------------------------------------------------------------------------
 }; // namespace android
