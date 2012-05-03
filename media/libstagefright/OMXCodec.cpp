d484298a2d08d21dbf4a45d6e528fbefdb433f86
Quarx2k <agent00791@gmail.com>
2012-05-03 14:24:18 +0700

    very ugly, but allow to start camera:)

--- media/libstagefright/OMXCodec.cpp
+++ media/libstagefright/OMXCodec.cpp
@@ -5886,4 +5886,20 @@ void OMXCodec::setQCELPFormat(int32_t numChannels, int32_t sampleRate, int32_t b
 }
 #endif
 
+extern "C" int  *_ZN7android11LiveSession6seekToEx()
+{
+    return _ZN7android11LiveSession6seekToEx();
+}
+
+extern "C" int  *_ZN7android14LiveDataSource16getValidDataSizeEv()
+{
+    return _ZN7android14LiveDataSource16getValidDataSizeEv();
+}
+
+extern "C" int  *_ZN7android14LiveDataSource3eofEv()
+{
+    return _ZN7android14LiveDataSource3eofEv();
+}
+
+
 }  // namespace android
