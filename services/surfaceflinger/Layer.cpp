1be1c8309de266e5e8dd4386300121e7c62d53e0
Quarx2k <agent00791@gmail.com>
2012-05-10 23:36:30 +0700

    only it need for defy

--- services/surfaceflinger/Layer.cpp
+++ services/surfaceflinger/Layer.cpp
@@ -657,11 +657,11 @@ uint32_t Layer::getEffectiveUsage(uint32_t usage) const
         // need a hardware-protected path to external video sink
         usage |= GraphicBuffer::USAGE_PROTECTED;
     }
-#ifdef MISSING_GRALLOC_BUFFERS
+//#ifdef MISSING_GRALLOC_BUFFERS
     usage |= GraphicBuffer::USAGE_HW_TEXTURE;
-#else
-    usage |= GraphicBuffer::USAGE_HW_COMPOSER;
-#endif
+//#else
+//    usage |= GraphicBuffer::USAGE_HW_COMPOSER;
+//#endif
     return usage;
 }
 
