5fc3fa082f6699711ca58816ebdff67b47e54c83
Quarx2k <agent00791@gmail.com>
2012-12-08 19:07:31 +0700

    add ro.wpa6_upnp_hack

--- wifi/java/android/net/wifi/WifiStateMachine.java
+++ wifi/java/android/net/wifi/WifiStateMachine.java
@@ -2738,8 +2738,10 @@ public class WifiStateMachine extends StateMachine {
             /* initialize network state */
             setNetworkDetailedState(DetailedState.DISCONNECTED);
 
-            /* set Rx filters to initial state */
-            mWifiNative.initializeRxFilters();
+            if (Boolean.parseBoolean(SystemProperties.get("ro.wpa6_upnp_hack", ""))) {
+                /* set Rx filters to initial state */
+                mWifiNative.initializeRxFilters();
+            }
 
             /* Remove any filtering on Multicast v6 at start */
             mWifiNative.stopFilteringMulticastV6Packets();
